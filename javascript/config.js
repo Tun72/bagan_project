76b17a690dfca20e3f5221fd89ca5cdb8b92c679
tun72 <tunt72553@gmai.com>
2023-03-19 01:55:49 +0630

    first commit

---
+++ javascript/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvVolSPQ0_mKAb1fZzsv1jzoEkExWDCk4",
+  authDomain: "baganproject-870c3.firebaseapp.com",
+  projectId: "baganproject-870c3",
+  storageBucket: "baganproject-870c3.appspot.com",
+  messagingSenderId: "385165614763",
+  appId: "1:385165614763:web:a6df6be188c1ad74a861ee",
+  measurementId: "G-3E1PZTJ1P6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ javascript/login.js
@@ -0,0 +1,5 @@
+const password = document.querySelector(".password");
+const email = document.querySelector("email");
+const signUpButtom = document.querySelector(".input-field").children;
+
+
